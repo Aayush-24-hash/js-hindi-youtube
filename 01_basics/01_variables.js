a6ba8d0348892ad062ca144fa3b3376b08cee561
Aayush Sahni <aayushsahni44@gmail.com>
2025-05-20 07:50:58 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 14567
+let accountEmail ="abcd@gmail.com"
+var accountPassword ="12345"
+accountCity = "Bhopal" // variable can be declared without help of any keyword but it is not preffered
+accountState;
+// accountId = 23434 // const values can not be changed
+accountEmail = "aayu89@gmail.com"
+accountPassword = "0987"
+accountCity = "New Delhi"
+
+console.log(accountId);
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState])
+
+/* 
+prefer not to use var because of issue in block scope
+and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";// treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs, not browser 
+
+console.log("Aayush")
+
+let name = "Aayush"
+let age = "19"
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned yet
+// symbol => unique
+
+
+//object
+
+
+console.log(typeof null)
